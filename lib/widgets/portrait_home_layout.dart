8b3f54d9006f090913137925c7f42b7fad4356c6
thpir <thijs.pirmez@gmail.com>
2023-04-26 13:54:34 +0200

    Continued home screen UI work

---
+++ lib/widgets/portrait_home_layout.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class PortraitHomeLayout extends StatelessWidget {
+  const PortraitHomeLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+            children: [
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 15),
+                    child: InkWell(
+                      onTap: () {
+                        
+                      },
+                      child: Card(
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        color: Theme.of(context).backgroundColor,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Stack(
+                              children: [
+                                Icon(
+                                  Icons.qr_code_scanner,
+                                  size: 150,
+                                  color: Theme.of(context).focusColor,
+                                ),
+                                Container(
+                                  width: 150,
+                                  height: 150,
+                                  alignment: Alignment.bottomRight,
+                                  child: const CircleAvatar(
+                                    radius: 20,
+                                    backgroundColor: Colors.black87,
+                                    child: Icon(
+                                      Icons.camera,
+                                      size: 35,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              'Scan',
+                              style: Theme.of(context).textTheme.headline6,
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 15, left: 30, right: 30, bottom: 30),
+                    child: InkWell(
+                      onTap: () {
+                        
+                      },
+                      child: Card(
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        color: Theme.of(context).backgroundColor,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Stack(
+                              children: [
+                                Icon(
+                                  Icons.qr_code,
+                                  size: 150,
+                                  color: Theme.of(context).focusColor,
+                                ),
+                                Container(
+                                  width: 150,
+                                  height: 150,
+                                  alignment: Alignment.bottomRight,
+                                  child: const CircleAvatar(
+                                    radius: 20,
+                                    backgroundColor: Colors.black87,
+                                    child: Icon(
+                                      Icons.add,
+                                      size: 35,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              'Create',
+                              style: Theme.of(context).textTheme.headline6,
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+  }
+}
